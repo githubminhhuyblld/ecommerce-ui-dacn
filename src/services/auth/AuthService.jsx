be5de2fe979da241acf7b520cd7777268e6b80b7
Nguyen Minh Huy <91535034+nhokkoihp01@users.noreply.github.com>
2023-06-09 17:49:24 +0700

    login api ++

---
+++ src/services/auth/AuthService.jsx
@@ -0,0 +1,72 @@
+import authHeader from "~/services/auth/authHeader.jsx";
+import instance from "~/interceptors/axios.jsx";
+
+const login = (username, password) => {
+    return instance
+        .post("/auth/login", {
+            username,
+            password,
+        })
+        .then((response) => {
+            if (response.data.accessToken) {
+                localStorage.setItem("token", JSON.stringify(response.data));
+            }
+
+            return response.data;
+        });
+};
+const register = async (username, password, email, firstName, lastName, numberPhone) => {
+    try {
+        const response = await instance.post("/auth/signup", {
+            username,
+            password,
+            email,
+            firstName,
+            lastName,
+            numberPhone
+        });
+        if (response.data.accessToken) {
+            return response;
+        } else {
+            return response
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const getInfoUser = () => {
+    return instance.get("/users/me", {headers: authHeader()});
+}
+const getCurrentUser = () => {
+    return JSON.parse(localStorage.getItem("token"))
+}
+const logout = () => {
+    localStorage.removeItem("token");
+};
+// const isTokenExpired = () => {
+//     const token = JSON.parse(localStorage.getItem("token"));
+//     if (!token || !token.accessToken) {
+//         return true;
+//     }
+//     const decodedToken = jwt_decode(token.accessToken);
+//     if (!decodedToken || !decodedToken.exp) {
+//         return true;
+//     }
+//     const expireTime = new Date(decodedToken.exp * 1000);
+//     const currentTime = new Date();
+//     return currentTime > expireTime;
+// };
+
+
+const AuthService = {
+    login,
+    getInfoUser,
+    getCurrentUser,
+    logout,
+    register,
+    // isTokenExpired
+
+};
+export default AuthService;
