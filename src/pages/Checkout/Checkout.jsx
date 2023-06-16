cbbd812c4545d39827217d380082ca44193cd8a0
Nguyen Minh Huy <91535034+nhokkoihp01@users.noreply.github.com>
2023-06-16 11:28:44 +0700

    remove,update cartItem ++

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,47 +1,25 @@
 import React, { useCallback, useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-import Product from "~/assets/product/product1.jpg";
 import CheckoutTable from "./CheckoutTable/CheckoutTable";
 import { Container } from "@mui/material";
 import {
-  getCartItems,
   selectCartItems,
-  setSuccess,
 } from "~/store/reducers/cartsSlice";
+
 
 Checkout.propTypes = {};
 
 function Checkout(props) {
-
-
-  const items = [
-    {
-      name: "Thời Trang JIVIVIL Túi Khoác Vai Dành Cho Nữ Phiên Bản Hàn Quốc Của Màu Trơn Túi Đeo Chéo Chất Liệu Da Pu",
-      image: Product,
-      quantity: 1,
-      newPrice: 3000000,
-    },
-    {
-      name: "Thời Trang JIVIVIL Túi Khoác Vai Dành Cho Nữ Phiên Bản Hàn Quốc Của Màu Trơn Túi Đeo Chéo Chất Liệu Da Pu",
-      image: Product,
-      quantity: 2,
-      newPrice: 3000000,
-    },
-    {
-      name: "Thời Trang JIVIVIL Túi Khoác Vai Dành Cho Nữ Phiên Bản Hàn Quốc Của Màu Trơn Túi Đeo Chéo Chất Liệu Da Pu",
-      image: Product,
-      quantity: 3,
-      newPrice: 3000000,
-    },
-  ];
+  const carts = useSelector(selectCartItems);
+  
 
   return (
     <div>
       <Container>
         <h3 className="text-5xl py-6 text-primary">Giỏ hàng</h3>
-        <CheckoutTable items={items} />
+        <CheckoutTable carts={carts} />
       </Container>
     </div>
   );
