f268f00eeb0e7e07a19174ee3996099d6285ec4f
Nguyen Minh Huy <91535034+nhokkoihp01@users.noreply.github.com>
2023-06-19 18:54:30 +0700

    up ++

--- src/layouts/components/ProductItem/ProductItem.jsx
+++ src/layouts/components/ProductItem/ProductItem.jsx
@@ -1,66 +1,82 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import classNames from "classnames/bind";
-import {Link} from "react-router-dom";
-import {useSelector} from "react-redux";
-import {Typography, Card, CardContent, Skeleton} from "@mui/material";
-
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Typography, Card, CardContent, Skeleton } from "@mui/material";
+import { animateScroll as scroll } from "react-scroll";
 
 import styles from "./ProductItem.module.scss";
-import {convertCurrency} from "~/untils/convertCurrency.js";
-import {selectLoading} from "~/store/reducers/productsSlice.js";
+import { convertCurrency } from "~/untils/convertCurrency.js";
+import { selectLoading } from "~/store/reducers/productsSlice.js";
 
 const cx = classNames.bind(styles);
 
 ProductItem.propTypes = {
-    product: PropTypes.object.isRequired,
+  product: PropTypes.object.isRequired,
 };
 
 function ProductItem(props) {
-    const {product} = props;
-    const loading = useSelector(selectLoading);
-    if (loading) {
-        return (
-            <Card>
-                <Skeleton variant="rectangular" width="100%" height={200} animation="wave"/>
-                <CardContent>
-                    <Skeleton height={20} animation="wave"/>
-                    <Skeleton height={20} animation="wave"/>
-                    <Skeleton height={20} animation="wave"/>
-                </CardContent>
-            </Card>
-        );
-    }
+  const { product } = props;
+  const navigate = useNavigate();
+  const loading = useSelector(selectLoading);
+  const handleProductClick = () => {
+    scroll.scrollToTop();
+  };
+  if (loading) {
     return (
-        <Link to={`/product-detail/${product.id}`} className={cx('wrapper')}>
-            <Card>
-                <div className={cx("product-image")}>
-                    <img src={product.mainImage} alt={product.name}/>
-                </div>
-                <CardContent>
-                    <Typography variant="h6" className={cx('product-name')}>
-                        {product.name}
-                    </Typography>
-                    <div className={cx('price')}>
-                        <Typography variant="body1">
-                            <span className={cx("new-price")}>{convertCurrency(product.newPrice)}</span>
-                        </Typography>
-                        <div>
-                            <Typography variant="body2">
-                                <span className={cx("old-price")}>{convertCurrency(product.oldPrice)}</span>
-                            </Typography>
-                            <Typography variant="body2">
-                                <span className={cx("sale")}>-{product.sale}</span>
-                            </Typography>
-                        </div>
-                        <Typography variant="body2">
-                           <span className={cx("quantity")}> Số lượng còn: {product.quantity}</span>
-                        </Typography>
-                    </div>
-                </CardContent>
-            </Card>
-        </Link>
+      <Card>
+        <Skeleton
+          variant="rectangular"
+          width="100%"
+          height={200}
+          animation="wave"
+        />
+        <CardContent>
+          <Skeleton height={20} animation="wave" />
+          <Skeleton height={20} animation="wave" />
+          <Skeleton height={20} animation="wave" />
+        </CardContent>
+      </Card>
     );
+  }
+  return (
+    <Link to={`/product-detail/${product.id}`} onClick={() => handleProductClick()} className={cx("wrapper")}>
+      <Card>
+        <div className={cx("product-image")}>
+          <img src={product.mainImage} alt={product.name} />
+        </div>
+        <CardContent>
+          <Typography variant="h6" className={cx("product-name")}>
+            {product.name}
+          </Typography>
+          <div className={cx("price")}>
+            <Typography variant="body1">
+              <span className={cx("new-price")}>
+                {convertCurrency(product.newPrice)}
+              </span>
+            </Typography>
+            <div>
+              <Typography variant="body2">
+                <span className={cx("old-price")}>
+                  {convertCurrency(product.oldPrice)}
+                </span>
+              </Typography>
+              <Typography variant="body2">
+                <span className={cx("sale")}>-{product.sale}</span>
+              </Typography>
+            </div>
+            <Typography variant="body2">
+              <span className={cx("quantity")}>
+                {" "}
+                Số lượng còn: {product.quantity}
+              </span>
+            </Typography>
+          </div>
+        </CardContent>
+      </Card>
+    </Link>
+  );
 }
 
 export default ProductItem;
