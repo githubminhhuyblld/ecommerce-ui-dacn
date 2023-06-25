df3bcb0a6c2bb57f6d445dda8dac6fdc83e3cc2b
hxthinh2001 <19130214@st.hcmuaf.edu.vn>
2023-06-25 23:25:48 +0700

    footer++

--- src/layouts/components/Footer/Footer.jsx
+++ src/layouts/components/Footer/Footer.jsx
@@ -267,10 +267,10 @@ function Footer(props) {
 
       <Container>
         <div className={cx("footer-third")}>
-          <div className={cx("lzd-footer-width-50 lzd-footer-left")}>
+          <div className={cx("lzd-footer-width-50")}>
             <div className={cx("lzd-footer-about")}>
               <h1>LAZADA - NỀN TẢNG MUA SẮM TRỰC TUYẾN HÀNG ĐẦU VIỆT NAM</h1>
-              <p style={{ marginBottom: "20px" }}>
+              <p style={{ marginBottom: "12px" }}>
                 <span
                   style={{
                     color: "rgb(136, 136, 136)",
@@ -287,7 +287,7 @@ function Footer(props) {
                   Đông Nam Á vào năm 2030.
                 </span>
               </p>
-              <p style={{ marginBottom: "20px" }}>
+              <p style={{ marginBottom: "12px" }}>
                 <span
                   style={{
                     color: "rgb(136, 136, 136)",
@@ -306,7 +306,7 @@ function Footer(props) {
                   bỏ lỡ của các tín đồ mua sắm trên toàn quốc.
                 </span>
               </p>
-              <p style={{ marginBottom: "20px" }}>
+              <p>
                 <span
                   style={{
                     color: "rgb(136, 136, 136)",
@@ -319,12 +319,199 @@ function Footer(props) {
                   trí tuyệt vời và siêu tiết kiệm ngay hôm nay!
                 </span>
               </p>
+              <h1>SẢN PHẨM CHÍNH HÃNG</h1>
+              <p
+                style={{
+                  color: "rgb(136, 136, 136)",
+                  backgroundColor: "rgb(244,244,246)",
+                  fontSize: "12px",
+                }}
+              >
+                Samsung , Apple , Xiaomi , Bluestone ,L’oreal , Maybeline , Enfa
+                , Bobby ,Huggies , Adidas , Casio , Mediamart ,Juno , Gumac
+              </p>
             </div>
           </div>
-          <div className={cx("lzd-footer-width-25")}></div>
-          <div className={cx("lzd-footer-width-25")}></div>
+          <div className={cx("lzd-footer-width-25")}>
+            <h3>SẢN PHẨM NỔI BẬT</h3>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>SMARTPHONE</h4>
+                <p>
+                  iPhone , Samsung , Xiaomi , Vivo , Vsmart , Oppo , Huawei ,
+                  iPhone 12 , iPhone 11 , Redmi Note 8 , Galaxy Note , Galaxy
+                  A51 , Galaxy S21 Ultra , Vsmart Live 4 , Vsmart Joy 3
+                </p>
+              </div>
+            </div>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>MÁY TÍNH BẢNG</h4>
+                <p>
+                  iPad , Samsung , Huawei , Lenovo , Kindle , iPad Pro , iPad
+                  mini , Masstel , iPad Air 2 , iPad Mini 5 , LAPTOPS , Dell ,
+                  Asus , Macbook , HP , Lenovo , Acer , MSI , Microsoft Surface
+                  , Macbook Pro , Macbook Air 2018
+                </p>
+              </div>
+            </div>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>TIVI</h4>
+                <p>
+                  Sony , LG , Vsmart , Xiaomi , Toshiba , Samsung , Panasonic ,
+                  Sharp , Asanzo , Asano , TCL
+                </p>
+              </div>
+            </div>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>ÂM THANH</h4>
+                <p>
+                  Loa Bluetooth , Loa Karaoke , JBL , Sony , Harman Kardon ,
+                  Bose , Tronsmart , Kiomic , Zealot , LG , Temeisheng , Hoco ,
+                  Kimiso
+                </p>
+              </div>
+            </div>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>TỦ LẠNH</h4>
+                <p>
+                  Tủ lạnh mini , Sanyo , Electrolux , Panasonic , Toshiba ,
+                  Samsung
+                </p>
+              </div>
+            </div>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>MỸ PHẨM & LÀM ĐẸP</h4>
+                <p>
+                  La Roche Posay , MAC , Maybelline , Laneige , Innisfree ,
+                  Vichy , Hadalabo , Senka , Anessa , The Face Shop , Son Black
+                  Rouge , Kiehl’s , Kem Chống Nắng , Sữa Rửa Mặt , Nước Tẩy
+                  Trang
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={cx("lzd-footer-width-25")}>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>MÁY ẢNH</h4>
+                <p>
+                  Fujifilm , Nikon , Canon , Sony , Camcorder , GoPro , Flycam ,
+                  Xiaomi
+                </p>
+              </div>
+            </div>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>MÁY GIẶT</h4>
+                <p>
+                  Electrolux , Sanyo , Toshiba , Aqua , Hitachi , Panasonic ,
+                  Samsung , LG , Bosch , Midea
+                </p>
+              </div>
+            </div>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>LÒ VI SÓNG</h4>
+                <p>
+                  Sharp , Electrolux , Bluestone , Samsung , Xiaomi , Beko ,
+                  Panasonic
+                </p>
+              </div>
+            </div>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>Ô TÔ XE MÁY</h4>
+                <p>
+                  Honda , Xe máy Yamaha , Taya Motor , Suzuki , Piaggio , Xe Máy
+                  Điện
+                </p>
+              </div>
+            </div>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>TRUYỆN TRANH HAY NHẤT NĂM 2021</h4>
+                <p>
+                  Thám tử lừng danh Conan , Thanh Gươm Diệt Quỷ Kimetsu Yaiba ,
+                  One Piece , Đảo Hải Tặc
+                </p>
+              </div>
+            </div>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>BÁCH HÓA ONLINE</h4>
+                <p>
+                  Thức Uống Có Cồn , Strongbow , Heineken , Tiger , Bia 333 ,
+                  Budweiser , Corona , Hoegaarden , Thức Uống Có Ga , Coca-cola
+                  , 7Up , Pepsi , La Vie , Star Kombucha
+                </p>
+              </div>
+            </div>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>XU HƯỚNG TÌM KIẾM NỔI BẬT 2021</h4>
+                <p>
+                  Sản phẩm mùa dịch Covid-19 , Khẩu trang 3M , Khẩu trang
+                  Unicharm , Khẩu trang y tế , Nước Rửa Tay Khô , Cà Phê , Nồi
+                  Cơm Điện , Giường Ngủ , Đồng Hồ Nữ , Đồng Hồ Nam , Máy Chạy Bộ
+                  Điện , Đàn Guitar , Đàn Piano , Ghế Mát Xa , Dép Crocs , Cờ
+                  Vua , Nón Sơn , Xe Cẩu , Xe Cẩu , Hoa , Bảng Chữ Cái , Đá Bóng
+                  , Áo Bóng Đá , Bikini , Elsa , BKAV , Ivy Moda , MLB , Dirty
+                  Coin , Quần Ống Rộng , Vascara
+                </p>
+              </div>
+            </div>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>MUA SẮM TRÊN TOÀN THẾ GIỚI VỚI ALIEXPRESS</h4>
+                <p>
+                  Nga , Tây Ban Nha , Pháp , Đức , Ba Lan , Brazil , Ả Rập Xê-út
+                </p>
+              </div>
+            </div>
+            <div className={cx("footer-outstanding")}>
+              <div>
+                <h4>MUA SẮM TRÊN TOÀN THẾ GIỚI VỚI DARAZ</h4>
+                <p>Pakistan , Bangladesh , Sri Lanka , Myanmar , Nepal</p>
+              </div>
+            </div>
+          </div>
         </div>
       </Container>
+
+      {/* footer fourth */}
+      <div className={cx("footer-fourth")}>
+        <div className={cx("lzd-footer-inner")}>
+          <div className={cx("lzd-footer-width-50")}>
+            <h3 className={cx("lzd-footer-title")}>Lazada Southeast Asia</h3>
+            <div className={cx("logo-country")}>
+              <img src="/src/assets/footer/indonesia.jpg" alt="" />
+              <img src="/src/assets/footer/malaysia.png" alt="" />
+              <img src="/src/assets/footer/philippines.png" alt="" />
+              <img src="/src/assets/footer/singapore.png" alt="" />
+              <img src="/src/assets/footer/thailan.png" alt="" />
+              <img src="/src/assets/footer/vietnam.png" alt="" />
+            </div>
+          </div>
+          <div className={cx("lzd-footer-width-25")}>
+            <h3 className={cx("lzd-footer-title")}>Kết nối với chúng tôi</h3>
+            <div className={cx("logo-connect")}>
+              <img src="/src/assets/footer/facebook.png" alt="" />
+              <img src="/src/assets/footer/in.png" alt="" />
+              <img src="/src/assets/footer/ytb.png" alt="" />
+              <img src="/src/assets/footer/insta.png" alt="" />
+              <img src="/src/assets/footer/tiktok.png" alt="" />
+            </div>
+          </div>
+          <div className={cx("lzd-footer-width-25")}>
+            <div className={cx("lzd-footer-copyright")}>© Lazada 2023</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
