f25adee9100a969eba56392e9e2226212ac19f01
Nguyen Minh Huy <91535034+nhokkoihp01@users.noreply.github.com>
2023-07-04 10:30:35 +0700

    edit ui ++

--- src/interceptors/axios.jsx
+++ src/interceptors/axios.jsx
@@ -1,29 +1,29 @@
 import axios from "axios";
 
-//  export const BASE_URL = "http://14.225.192.194:8080/ecommerce-api/api/v1"
-export const BASE_URL = "https://ecommerce-dacn-service.onrender.com/ecommerce-api/api/v1"
+export const BASE_URL = "https://ecommerce-dacn.site:8443/ecommerce-api/api/v1";
+// export const BASE_URL = "https://ecommerce-dacn-service.onrender.com/ecommerce-api/api/v1"
 // export const BASE_URL= "http://localhost:8087/ecommerce-api/api/v1"
 const PROVINCES_URL = "https://provinces.open-api.vn/api";
 const instance = axios.create({
-    baseURL: BASE_URL,
-    headers: {"Content-Type": "application/json"},
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
 });
 export const provinces = axios.create({
-    baseURL: PROVINCES_URL,
-    headers: {"Content-Type": "application/json"},
+  baseURL: PROVINCES_URL,
+  headers: { "Content-Type": "application/json" },
 });
 instance.interceptors.request.use(async (config) => {
-    const accessToken = localStorage.getItem("token");
-    if (accessToken) {
-        const {headers} = config;
-        return {
-            ...config,
-            headers: {
-                ...headers,
-                "Access-Token": `Token ${accessToken}`,
-            },
-        };
-    }
-    return config;
+  const accessToken = localStorage.getItem("token");
+  if (accessToken) {
+    const { headers } = config;
+    return {
+      ...config,
+      headers: {
+        ...headers,
+        "Access-Token": `Token ${accessToken}`,
+      },
+    };
+  }
+  return config;
 });
 export default instance;
--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -1,310 +1,124 @@
-import {Container, FormControl, Grid, InputLabel, MenuItem, Select, TextField} from "@mui/material";
-import styles from "~/pages/EditProfile/EditProfile.module.scss"
+import { useEffect, useState } from "react";
+import { IconButton, InputAdornment, TextField } from "@material-ui/core";
 import classNames from "classnames/bind";
-import {Link} from "react-router-dom";
-import {useEffect, useState} from "react";
-import {styled} from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import {orange, purple} from '@mui/material/colors';
+import { Link } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import { Container, Grid, Select, MenuItem } from "@mui/material";
+import moment from "moment";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
+import "dayjs/locale/vi";
+
+import styles from "~/pages/EditProfile/EditProfile.module.scss";
 import config from "~/config/index.jsx";
-import {useSelector} from "react-redux";
-import {selectUser} from "~/store/reducers/userSlice.js";
+import { selectUser } from "~/store/reducers/userSlice.js";
+import SidebarLeft from "~/layouts/components/SidebarLeft/SidebarLeft";
 
 const cx = classNames.bind(styles);
+dayjs.locale("vi");
 
 function EditProfile() {
+  const [gender, setGender] = useState("MALE");
+  const [selectedDate, setSelectedDate] = useState(null);
+  console.log(selectedDate);
+  const handleDateChangeBirthDay = (date) => {
+    setSelectedDate(date.valueOf());
+  };
 
-    const [month, setMonth] = useState("")
-    const [day, setDay] = useState("")
-    const [year, setYear] = useState("")
+  const handleChangeGender = (e) => {
+    setGender(e.target.value);
+  };
 
-    const user = useSelector(selectUser)
-    console.log(user)
-
-    const [lastName, setLastName] = useState("")
-    const [firstName, setFirstname] = useState("")
-    const [gender, setGender] = useState("")
-
-    const email = user !== null && user?.email
-    const numberPhone = user !== null && user?.numberPhone
-
-    useEffect(() => {
-        setLastName(user !== null && user?.lastName)
-        setFirstname(user !== null && user?.firstName)
-        setGender(user !== null && user?.gender)
-        setDay(user !== null && new Date(user?.dateOfBirth).getDate())
-        setMonth(user !== null && (new Date(user?.dateOfBirth).getMonth() + 1))
-        setYear(user !== null && new Date(user?.dateOfBirth).getFullYear())
-    }, [user])
-
-    const handleChangeMonth = (e) => {
-        setMonth(e.target.value)
-    }
-
-    const handleChangeDay = (e) => {
-        setDay(e.target.value)
-    }
-
-    const handleChangeYear = (e) => {
-        setYear(e.target.value)
-    }
-
-    const handleChangeSex = (e) => {
-        setGender(e.target.value)
-    }
-
-    const listDay = createDay()
-
-    function createDay() {
-        const result = []
-        for (let i = 1; i <= 31; i++) {
-            result[i - 1] = i
-        }
-        return result
-    }
-
-    const listMonth = createMonth()
-
-    function createMonth() {
-        const result = []
-        for (let i = 1; i <= 12; i++) {
-            result[i - 1] = i
-        }
-        return result
-    }
-
-    const listYear = createYear()
-
-    function createYear() {
-        const result = []
-        let date = new Date()
-        let index = 0;
-        for (let i = date.getFullYear(); i >= 1900; i--) {
-            result[index] = i
-            index++
-        }
-        return result
-    }
-
-    const listSex = [
-        "MALE",
-        "FEMALE"
-    ]
-    const ColorButton = styled(Button)(({theme}) => ({
-        color: theme.palette.getContrastText(purple[500]),
-        backgroundColor: orange[700],
-        width: "170%",
-        fontSize: "70%",
-        '&:hover': {
-            backgroundColor: orange[800],
-        },
-    }));
-    const sidebarLeft = [
-        {
-            id: 1,
-            title: "Quản lý tài khoản",
-            children: [
-                {id: 1, name: "Thông tin cá nhân", to: config.routes.account},
-                {id: 2, name: "Số địa chỉ", to: ""},
-                {id: 3, name: "  Tùy chọn thanh toán", to: ""},
-            ],
-        },
-        {
-            id: 2,
-            title: "Đơn hàng của tôi",
-            children: [
-                {id: 1, name: "Đơn hàng đổi trả", to: ""},
-                {id: 2, name: "Đona hàng hủy", to: ""},
-            ],
-        },
-        {
-            id: 2,
-            title: "Nhận xét của tôi",
-            children: [],
-        },
-        {
-            id: 2,
-            title: "Sản phẩm yêu thích & Gian hang đang theo dõi",
-            children: [],
-        },
-    ];
-
-    return (
-        <div className="w-full bg-gray-200 p-8">
-            <Container>
-                <div className="grid grid-cols-12 gap-4 py-12">
-                    <div className="md:col-span-3 px-2 lg:col-span-2 hidden md:block">
-                        {sidebarLeft.map((item, index) => {
-                            return (
-                                <div key={index}>
-                                    <h3 className="md:text-2xl lg:text-3xl">{item.title}</h3>
-                                    <ul className="px-6 py-6 flex flex-col">
-                                        {item.children.map((child, index) => {
-                                            return (
-                                                <Link
-                                                    to={child.to}
-                                                    key={index}
-                                                    className="text-gray-500 text-2xl py-2"
-                                                >
-                                                    {child.name}
-                                                </Link>
-                                            );
-                                        })}
-                                    </ul>
-                                </div>
-                            );
-                        })}
+  return (
+    <div className="w-full bg-gray-200 p-8">
+      <Container>
+        <div className="grid grid-cols-12 gap-4 py-12">
+          <div className="md:col-span-3 px-2 lg:col-span-2 hidden md:block">
+            <SidebarLeft />
+          </div>
+          <div className="col-span-12 md:col-span-9 lg:col-span-10 sm:col-span-12">
+            <h3 className="text-4xl mb-4">Chỉnh sửa thông tin cá nhân</h3>
+            <div className="bg-white py-12 px-8 rounded-lg">
+              <Grid container spacing={2}>
+                <Grid item lg={4} md={12} sm={12} xs={12}>
+                  <div>
+                    <label className="text-2xl mb-2">Họ và tên</label>
+                    <TextField
+                      name="name"
+                      className="input-field"
+                      placeholder="Họ và Tên"
+                      type="text"
+                      fullWidth
+                      margin="normal"
+                    />
+                    <span className="error"></span>
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="text-2xl mb-2">Giới tính</label>
+                    <Select
+                      className="select-field"
+                      value={gender}
+                      onChange={handleChangeGender}
+                    >
+                      <MenuItem value="MALE">Nam</MenuItem>
+                      <MenuItem value="FEMALE">Nữ</MenuItem>
+                    </Select>
+                  </div>
+                  <div className="flex flex-col">
+                    <label className="text-2xl mb-2">Ngày sinh</label>
+                    <LocalizationProvider
+                      dateAdapter={AdapterDayjs}
+                      locale="vi"
+                    >
+                      <DatePicker
+                        value={selectedDate}
+                        onChange={handleDateChangeBirthDay}
+                        className="datepicker-container"
+                        inputClassName="datepicker-input"
+                        labelClassName="datepicker-label"
+                        format="DD/MM/YYYY"
+                        fullWidth
+                      />
+                    </LocalizationProvider>
+                  </div>
+                </Grid>
+                <Grid item lg={4} md={12} sm={12} xs={12}>
+                  <div className="flex flex-col">
+                    <div>
+                      <label className="text-2xl">Số điện thoại</label>
+                      <TextField
+                        name="name"
+                        className="input-field"
+                        placeholder="Số điện thoại"
+                        type="number"
+                        fullWidth
+                        margin="normal"
+                      />
+                      <span className="error"></span>
                     </div>
-                    <div className="col-span-12 md:col-span-9 lg:col-span-10 sm:col-span-12">
-                        <h2 className={cx('title')}>
-                            Chỉnh sửa
-                        </h2>
-                        <Grid className={cx("frame")} style={{margin: "0 auto"}} container item spacing={2} xs={12}>
-                            <Grid container item xs={12}>
-                                <Grid item
-                                      xl={5}
-                                      lg={5}
-                                      md={12}
-                                      sm={12}
-                                      xs={12}>
-                                    <p className={"text-lg text-zinc-600"}>Họ tên</p>
-                                    <FormControl sx={{m: 1}} size="small">
-                                        <TextField
-                                            value={lastName}
-                                            onChange={(e) => {
-                                                setLastName(e.target.value)
-                                            }
-                                            }
-                                            placeholder={"Họ"}
-                                            size="small"
-                                        />
-                                    </FormControl>
-                                    <FormControl sx={{m: 1}} size="small">
-                                        <TextField
-                                            value={firstName}
-                                            onChange={(e) => {
-                                                setFirstname(e.target.value)
-                                            }
-                                            }
-                                            placeholder={"Tên"}
-                                            size="small"
-                                        />
-                                    </FormControl>
-                                </Grid>
-                                <Grid item xl={4}
-                                      lg={4}
-                                      md={6}
-                                      sm={6}
-                                      xs={12}>
-                                    <p className="flex items-center text-lg text-zinc-600">Địa chỉ email<span
-                                        className={cx('hr')}>|</span><Link
-                                        className={cx('link')}
-                                        to={""}>Thay đổi</Link></p>
-                                    <div className={"text-xl"}>{email}</div>
-                                </Grid>
-                                <Grid item xl={3}
-                                      lg={3}
-                                      md={6}
-                                      sm={6}
-                                      xs={12}>
-                                    <p className={"text-lg text-zinc-600"}>Số điện thoại<span
-                                        className={cx('hr')}>|</span><Link
-                                        className={cx('link')}
-                                        to={""}>Thêm</Link></p>
-                                    <div className={"text-xl"}>
-                                        {numberPhone}
-                                    </div>
-                                </Grid>
-                            </Grid>
-                            <Grid container item xs={12}>
-                                <Grid item xl={5}
-                                      lg={5}
-                                      md={12}
-                                      sm={12}
-                                      xs={12}>
-                                    <p className={"text-lg text-zinc-600"}>Ngày sinh</p>
-                                    <FormControl sx={{m: 1, minWidth: 120}} size="small">
-                                        <InputLabel id="month">Month</InputLabel>
-                                        <Select
-                                            labelId="month"
-                                            value={month}
-                                            label="Month"
-                                            onChange={handleChangeMonth}
-                                        >
-                                            {listMonth.map((month, index) => (
-                                                <MenuItem key={index} value={month}>{month}</MenuItem>
-                                            ))}
-                                        </Select>
-                                    </FormControl>
-                                    <FormControl sx={{m: 1, minWidth: 60}} size="small">
-                                        <InputLabel id="day">Day</InputLabel>
-                                        <Select
-                                            labelId="day"
-                                            value={day}
-                                            label="Day"
-                                            onChange={handleChangeDay}
-                                        >
-                                            {listDay.map((day, index) => (
-                                                <MenuItem key={index} value={day}>{day}</MenuItem>
-                                            ))}
-                                        </Select>
-                                    </FormControl>
-                                    <FormControl sx={{m: 1, minWidth: 80}} size="small">
-                                        <InputLabel id="year">Year</InputLabel>
-                                        <Select
-                                            labelId="year"
-                                            value={year}
-                                            label="Year"
-                                            onChange={handleChangeYear}
-                                        >
-                                            {listYear.map((year, index) => (
-                                                <MenuItem key={index} value={year}>{year}</MenuItem>
-                                            ))}
-                                        </Select>
-                                    </FormControl>
-                                </Grid>
-                                <Grid item xl={4}
-                                      lg={4}
-                                      md={6}
-                                      sm={6}
-                                      xs={12}>
-                                    <p className={"text-lg text-zinc-600"}>Giới tính</p>
-                                    <FormControl sx={{m: 1, minWidth: 100}} size="small">
-                                        <Select
-                                            value={gender}
-                                            onChange={handleChangeSex}
-                                        >
-                                            {listSex.map((year, index) => (
-                                                <MenuItem key={index} value={year}>{year}</MenuItem>
-                                            ))}
-                                        </Select>
-                                    </FormControl>
-                                </Grid>
-                                <Grid item xl={3}
-                                      lg={3}
-                                      md={6}
-                                      sm={6}
-                                      xs={12}>
-                                    <p className={"text-lg text-zinc-600"}>Mã số thuế</p>
-                                    <FormControl sx={{m: 1, minWidth: 120}} size="small">
-                                        <TextField
-                                            size="small"
-                                            placeholder={"Nhập mã số thuế của bạn"}
-                                        />
-                                    </FormControl>
-                                </Grid>
-                            </Grid>
-                            <Grid container item xs={12}>
-                                <Grid item>
-                                    <ColorButton variant="contained">Lưu thay đổi</ColorButton>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </div>
-                </div>
-            </Container>
+                  </div>
+                </Grid>
+                <Grid item lg={4} md={12} sm={12} xs={12}>
+                  <div className="flex flex-col">
+                    <span className="text-2xl">Địa chỉ Email</span>
+                    <span className="font-bold">minhhuy1222001@gmail.com</span>
+                  </div>
+                </Grid>
+              </Grid>
+              <div className="mt-12">
+                <button className="py-6 px-16 uppercase hover:bg-green-700 bg-green-600 text-white rounded-3xl">
+                  Lưu
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </Container>
+    </div>
+  );
 }
 
-export default EditProfile
+export default EditProfile;
